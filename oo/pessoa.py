9caefb7d21a1918fde58815008278be1a523cd9f
VGCafisso <cafisso@gmail.com>
2022-05-06 15:57:02 -0400

    Criado atributo complexo Filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if  __name__ == '__main__':
-    p = Pessoa('Vinicius')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Cafisso'
-    print(p.nome)
-    print(p.idade)
+    cafisso = Pessoa(nome='Cafisso')
+    vinicius = Pessoa(cafisso, nome='Vinicius')
+    print(Pessoa.cumprimentar(vinicius))
+    print(id(vinicius))
+    print(vinicius.cumprimentar())
+    print(vinicius.nome)
+    print(vinicius.idade)
+    for filho in vinicius.filhos:
+        print(filho.nome)
